f08ade0fd647d983df7c79c076efc2807a360f05
NSihr <91257590+NSihr@users.noreply.github.com>
2021-09-24 09:51:11 +0530

    Add files via upload

---
+++ Ncert/ques9phy.js
@@ -0,0 +1,222 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What do the following measure in a car? 1. Speedometer 2. Odometer",
+    answer: "1. Speed 2. Distance travelled",
+    options: [
+      "1. Speed 2. Distance travelled",
+      "1. Distance travelled 2. Speed",
+      "1. rainfall 2. time",
+      "1. Displacement 2. Pressure "
+    ]
+  },
+    {
+    numb: 2,
+    question: "Name the phycial quantity which gives idea about how slow or fast a body is moving.",
+    answer: "Speed",
+    options: [
+      "Distance travelled",
+      "Speed",
+      "Displacement",
+      "Velocity"
+    ]
+  },
+    {
+    numb: 3,
+    question: "In addition to speed, what else is needed to predict a object's position",
+    answer: "Direction of speed",
+    options: [
+      "Distance travelled",
+      "Displacment",
+      "Speed",
+      "Direction of speed"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What type of motion is exhibited by a free falling body?",
+    answer: "Accelerated Motion",
+    options: [
+      "Accelerated Motion",
+      "Uniform Motion",
+      "Circular Motion",
+      "Uniform speed"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The numerical ratio of displacement to distance for moving object is:",
+    answer: "equal to or less than 1",
+    options: [
+      "equal to or more than one",
+      "equal to or less than 1",
+      "always less than 1",
+      "always more than 1"
+    ]
+  },
+     {
+     numb: 6,
+     question: "A boy is sitting on a merry-go-round which is moving with a constant speed of 10m/s. This means the boy is:",
+     answer: "in accelerated motion",
+     options: [
+      "at rest",
+      "in uniform motion",
+      "in uniform velocity",
+      "in acclerated motion"
+     ]
+   },
+   {
+    numb: 7,
+    question: " The speed of a moving object is 0.06m/s.This speed is equal to:",
+    answer: "0.216km/h",
+    options: [
+     "2.16km/h",
+     "1.08km/h",
+     "0.216hm/h",
+     "0.0216m/h"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which of the following is correct regarding velocity and speed of a moving body-",
+    answer: "velocity of a moving body is speed in a given direction",
+    options: [
+     "velocity of a moving body is always higher than its speed ",
+     "velocity of a moving body is speed in a given diretion ",
+     "speed of a moving body is velocity in a given direction ",
+     " speed of a moving body is always higher than its velocity"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which of the following could not be a unit of speed?",
+    answer: "s/m",
+    options: [
+     "km/h",
+     "mm/s",
+     "s/m",
+     "m/s"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which of the following could not be a unit of acceleration?",
+    answer: "cm/s",
+    options: [
+     "km/s^2",
+     "m/s^2",
+     "cm/s",
+     "cm/s^2"
+    ]
+  },
+  {
+    numb: 11,
+    question: "What does not changes continuously in uniform circular motion?",
+    answer: "speed",
+    options: [
+     "speed",
+     "velocity",
+     "accleration",
+     "direction of motion"
+    ]
+  },
+  {
+    numb: 12,
+    question: "Name the quantity which is measured by the area occupied under the v-t graph.",
+    answer: "Distance travelled",
+    options: [
+     "force",
+     "velocity",
+     "speed",
+     "Distance travelled"
+    ]
+  },
+  {
+    numb: 13,
+    question: "What does the slope of speed time graph indicate?",
+    answer: "Acceleration",
+    options: [
+     "Speed",
+     "Acceleration",
+     "Distance travelled",
+     "Force"
+    ]
+  },
+  {
+    numb: 14,
+    question: "2as=v^2-u^2 what does s stand here for?",
+    answer: "displacement",
+    options: [
+     "speed",
+     "displacement",
+     "distance",
+     "velocity"
+    ]
+  },
+  {
+    numb: 15,
+    question: "if body moves with a unifom velocity, its accleration is",
+    answer: "zero",
+    options: [
+     "uniform",
+     "non uniform",
+     "zero",
+     "qual to velocity"
+    ]
+  },
+  {
+    numb: 16,
+    question: "The slope of a distance time indicates ________ of a body",
+    answer: "speed",
+    options: [
+     "work done",
+     "displacement",
+     "distance",
+     "speed"
+    ]
+  },
+  {
+    numb: 17,
+    question: "s=v^2-u^2/2a is this equation correct? ",
+    answer: "true",
+    options: [
+     "true",
+     "false"
+    
+    ]
+  },
+  {
+    numb: 18,
+    question: "If the displacement of a object is proportional to time^2, then object is moving with:",
+    answer: "uniform accleration",
+    options: [
+     "uniform speed",
+     "uniform accleration",
+     "uniform velocity",
+     "non uniform speed"
+    ]
+  },
+  {
+    numb: 19,
+    question: "It is possible for something to accelerate but not change its speed if its moving in a ",
+    answer: "circular motion",
+    options: [
+     "circular motion",
+     "linear motion",
+     "uniform motion",
+     "non uniform motion"
+    ]
+  },
+  {
+    numb: 20,
+    question: "Earth moves round the sun with uniform velocity",
+    answer: "False",
+    options: [
+     "True",
+     "False"
+    ]
+  },
+
+  
+];
